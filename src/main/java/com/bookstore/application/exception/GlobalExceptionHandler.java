dc17dafe0aac7aa39fe2330e41bea1fc0b6877c0
Vinod Sharma <vinu.2200@gmail.com>
2025-10-21 08:02:42 +0200

    Added logs

--- src/main/java/com/bookstore/application/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bookstore/application/exception/GlobalExceptionHandler.java
@@ -2,17 +2,20 @@ package com.bookstore.application.exception;
 
 import com.bookstore.application.domain.pojo.ErrorMessage;
 import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @Hidden
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BooksNotFoundException.class)
     public ResponseEntity<ErrorMessage> handleResourceNotFound(BooksNotFoundException ex) {
+        log.error("GlobalExceptionHandler#handleResourceNotFound::Message \n ### {}", ex.getMessage());
         return new ResponseEntity<>(ErrorMessage.builder()
                 .code("EMPT404")
                 .message(ex.getMessage())
@@ -22,6 +25,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorMessage> handleGeneralException(Exception ex) {
+        log.error("GlobalExceptionHandler#handleGeneralException::Message \n ### {}", ex.getMessage());
         return new ResponseEntity<>(ErrorMessage.builder()
                 .code("ERROR500")
                 .message(ex.getMessage())
